67f18a981ee52f2be0d7fafaa23c86c7feea146f
Guilherme Maciel <guilhermemcl1503@gmail.com>
2025-03-03 08:37:30 -0300

    Initialize project configuration and dependencies with env and zod validation data

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import { app } from './app'
+import { env } from './env'
+
+
+
+
+app
+    .listen({
+        host: '0.0.0.0',
+        port: env.PORT,
+    })
+    .then(() => {
+        console.log('🚀 Server is running on http://localhost:', env.PORT)
+    })
